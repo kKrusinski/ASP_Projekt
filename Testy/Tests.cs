815b634013d31fd517a3845ffca13211f4820550
Kacper Krusinski <kacper.krusinski@microsoft.wsei.edu.pl>
2022-01-28 10:36:44 +0100

    testy

--- Testy/Tests.cs
+++ Testy/Tests.cs
@@ -7,7 +7,7 @@ using Xunit;
 
 namespace Tests
 {
-    public class UnitTest1
+    public class Tests
     {
         [Fact]
         public void Test1()
@@ -25,6 +25,53 @@ namespace Tests
 
 
                 Assert.Equal(expected.Count, actual.Count);
+            }
+        }
+        [Fact]
+        public void Test2()
+        {
+
+            using (var mock = AutoMock.GetLoose())
+            {
+
+
+
+                RentalEquipment expected = GetItem(1);
+
+                mock.Mock<IRentalService>().Setup(x => x.GetItem(1)).Returns(GetItem(1));
+
+                var cls = mock.Create<IRentalService>();
+
+                var actual = cls.GetItem(1);
+
+
+                Assert.Equal(expected, actual);
+            }
+        }
+        [Fact]
+        public void Test3()
+        {
+
+            using (var mock = AutoMock.GetLoose())
+            {
+
+
+
+                RentalEquipment expected = new RentalEquipment
+                {
+                    Name = "Koparka CAT",
+                    BaseRentPrice = 250.00,
+                    DailyRentPrice = 75.00,
+                    OverdueRate = 15,
+                    IsAvailable = false
+                };
+
+                mock.Mock<IRentalService>().Setup(x => x.AddEquipment(expected));
+
+                var cls = mock.Create<IRentalService>();
+
+                cls.AddEquipment(expected);
+
             }
         }
         private List<RentalEquipment> GetAll()
@@ -59,6 +106,19 @@ namespace Tests
 
         };
             return list;
+        }
+
+        private RentalEquipment GetItem(int id)
+        {
+            List<RentalEquipment> list = GetAll();
+            foreach (var item in list)
+            {
+                if (item.ID == id)
+                {
+                    return item;
+                }
+            }
+            return null;
         }
 
     }
